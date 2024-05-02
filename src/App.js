fd374c3d9e41d915d451dd598f8ea185eed37e97
Kyle Uy <kyleeeee09@gmail.com>
2024-05-03 01:11:16 +0800

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./pages/landingPage/landingPage";
+import LoginPage from "./pages/loginPage/loginPage";
 
 function App() {
   return (
     <>
-      <LandingPage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </Router>
     </>
   );
 }
