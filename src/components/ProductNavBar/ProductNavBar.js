02634e23256d4680301767b6a9d30f93feb8dea6
rjcepe <cepenate@gmail.com>
2024-05-04 00:28:44 +0800

    products card fix

--- src/components/ProductNavBar/ProductNavBar.js
+++ src/components/ProductNavBar/ProductNavBar.js
@@ -4,8 +4,6 @@ import "./ProductNavBar.css";
 const ProductNavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg nav-underline navbar-dark ">
-      <div className="container-fluid">
-        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item mx-5 px-5">
               <Link className="nav-link active" aria-current="page" >Nutrition</Link>
@@ -23,8 +21,7 @@ const ProductNavBar = () => {
               <Link className="nav-link" >Personal Care</Link>
             </li>
           </ul>
-        </div>
-      </div>
+
     </nav>
   );
 };
